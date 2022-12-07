32936e2a88ce4ac8da094322f5634574a56dae01
Nutsu31 <116025262+Nutsu31@users.noreply.github.com>
2022-12-07 20:05:38 +0400

    update start properties

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const numOfPeople = document.getElementById('numOfPeople');
 
 
 
-numOfPeople.value = 1;
+tipSum.innerHTML = "$" + "0.00"
+totalSum.innerHTML = "$" + "0.00"
+numOfPeople.value = '1';
+
 reset.addEventListener('click',() => {
     tipSum.innerHTML = "$" + "0.00"
     totalSum.innerHTML = "$" + "0.00"
@@ -32,66 +35,3 @@ buttonValue.forEach(item => {
         totalSum.innerHTML = bill.value / numOfPeople.value + result / numOfPeople.value + "$"
     })
 });
-
-
-// function buttonTip (i) {
-//     buttonValue[i] = buttonValue.value
-//     let result = (bill.value * buttonTip[i].value)/ 100;
-//     tipSum.innerHTML = result + '$';
-//     totalSum.innerHTML =  bill.value / numOfPeople.value + result / numOfPeople.value + "$";
-// }
-
-// numOfPeople.value = 1;
-// tipAmount.innerHTML = "$"+ "0.00"
-// totalSum.innerHTML = "$"+ "0.00"
-
-
-// reset.addEventListener('click',()=>{
-//     payment.value = '';
-//     numOfPeople.value = 1;
-//     tipAmount.innerHTML = "$"+"0.00"
-//     totalSum.innerHTML = "$"+ "0.00"
-// })
-
-// function tipCustom () {
-//     let result = (payment.value * customTip.value) / 100;
-//     tipAmount.innerHTML = result + "$"
-//     totalSum.innerHTML = payment.value / numOfPeople.value + result / numOfPeople.value + "$"
-// }
-
-
-// buttonTip[0].addEventListener('click',()=> {
-//     let result = (payment.value * buttonTip[0].value)/ 100;
-//     tipAmount.innerHTML = result + '$';
-//     totalSum.innerHTML =  payment.value / numOfPeople.value + result / numOfPeople.value + "$";
-//     buttonTip[0].className = 'active'
-    
-// })
-// buttonTip[1].addEventListener('click',()=> {
-//     let result = (payment.value * buttonTip[1].value)/ 100;
-//     tipAmount.innerHTML = result + '$';
-//     totalSum.innerHTML = payment.value / numOfPeople.value + result / numOfPeople.value + "$";
-
-
-    
-// })
-// buttonTip[2].addEventListener('click',()=> {
-//     let result = (payment.value * buttonTip[2].value)/ 100;
-//     tipAmount.innerHTML = result + '$';
-//     totalSum.innerHTML = payment.value / numOfPeople.value + result / numOfPeople.value + "$";
-    
-// })
-// buttonTip[3].addEventListener('click',()=> {
-//     let result = (payment.value * buttonTip[3].value)/ 100;
-//     tipAmount.innerHTML = result + '$';
-//     totalSum.innerHTML = payment.value / numOfPeople.value + result / numOfPeople.value + "$";
-    
-// })
-// buttonTip[4].addEventListener('click',()=> {
-//     let result = (payment.value * buttonTip[4].value)/ 100;
-//     tipAmount.innerHTML = result + '$';
-//         totalSum.innerHTML = payment.value / numOfPeople.value + result / numOfPeople.value + "$";
-    
-// })
-
-
